90df75ddd01a33b6b0e63865e140ef9430ebeac4
Vaishnavi GP <gp.vaishnavi2003@gmail.com>
2025-05-06 09:23:02 +0530

    commit

--- com/test/Program1.java
+++ com/test/Program1.java
@@ -1,10 +1,10 @@
 package com.test;
 
-public class Program_1 {
+public class Program1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		system.out.println
 
 	}
 
