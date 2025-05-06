bf2f3d0d91b53de3dcb6b668b89dafc464954b22
vaish-navi22 <151398653+vaish-navi22@users.noreply.github.com>
2025-05-06 09:31:55 +0530

    Update Program_1.java

--- src/com/test/Program_1.java
+++ src/com/test/Program_1.java
@@ -5,6 +5,11 @@ public class Program_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		Sacnner s=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n=s.nextInt();
+		int res=n*n;
+		System.out.println("Square of number"+res);
 
 	}
 
